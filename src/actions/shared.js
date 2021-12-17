f3f190abb22ea3387b1ab9775437451eb9ed68f2
Mohammad <moh.osama84x@gmail.com>
2021-12-17 23:41:31 +0200

    adding answer action in user,handling in shared

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,6 +2,11 @@ import { getInitialData } from "../utils/api";
 import { recieveUsers } from "./users";
 import { recieveQuestions } from "./questions";
 import { showLoading , hideLoading } from "react-redux-loading";
+import { addAnswerUser } from "./users";
+import { submitAnswerQuestion } from "./questions";
+import { saveQuestionAnswer } from '../utils/api';
+
+
 
 export function handleInitialData (){
     return (dispatch)=>{
@@ -17,3 +22,12 @@ export function handleInitialData (){
         })
     }
 }
+
+
+export function handleQuestionAnswer( authedUser, qid , answer ) {
+                return (dispatch)=>{
+                    dispatch(addAnswerUser( authedUser, qid , answer ))
+                    dispatch(submitAnswerQuestion( authedUser, qid , answer ))            
+                return saveQuestionAnswer( authedUser, qid , answer )
+                }
+  }
--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,25 @@
 export const RECIEVE_USERS = "RECIEVE_USERS"
-
 export function recieveUsers (users) {
     return {
         type : RECIEVE_USERS , 
         users ,
     }
 }
+
+export const ADD_ANSWER_USER = 'ADD_ANSWER_USER';
+
+export function addAnswerUser( authedUser, qid , answer ) {
+    return {
+      type: ADD_ANSWER_USER,
+      authedUser,
+      qid,
+      answer
+    };
+  }
+
+
+
+
+
+
+
