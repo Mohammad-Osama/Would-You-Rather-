db0780a4b3f9d6e152481fec1ce974d35c99d60d
Mohammad <moh.osama84x@gmail.com>
2021-12-17 06:14:34 +0200

    form and input in ViewQuesion component

--- src/components/ViewQuesion.js
+++ src/components/ViewQuesion.js
@@ -1,52 +1,70 @@
-import React from 'react'
+import React  , {useState} from 'react'
 import { connect } from 'react-redux'
 import { useLocation } from 'react-router-dom'
-import { Button, Card, Image, PlaceholderImage } from 'semantic-ui-react'
+import { Button, Card, Image, Radio ,Label, Input } from 'semantic-ui-react'
 import { Message } from 'semantic-ui-react'
+import { Form, Checkbox } from 'semantic-ui-react'
+
 
 
 
    function ViewQuesion() {
+    
+    const [option, setOption] = React.useState();
+    const handleChange = e => {
+      const target = e.target;
+      if (target.checked) {
+        setOption(target.value);
+      }
+    };
+    const handleSubmit = e => {
+      e.preventDefault();
+      console.log(option);
+    };
+  
     const location = useLocation()
     const { status , CurrentQuestion , img } = location.state
     console.log (status + CurrentQuestion.id + img )
     console.log (location.state)
+    
+    
+    // onsubmit function  ? 
     // console.log (this.props )
-            if (status==="anwsered") 
-                  return (
-                 
-                    <Card >
-                    <Card.Content compact>  
-                        <Image src={img} floated="left" size='small' circular />
-
-                    
-                    
-                        <Card.Header>{CurrentQuestion.author +' asks'}</Card.Header>
-                        <Card.Description>                          
-                          <Message compact size='large'>
-                                    {CurrentQuestion.optionOne.text }                                         
-                                    </Message>
-                            <Card.Meta >Or</Card.Meta>
-                                   
-                        </Card.Description>
-                           
-                                <Button  fluid  basic color='green'
-                                                                  >
-                                   submit ur answer 
-                                </Button>
-                          
-                        
-                   
-                    </Card.Content>
-                </Card>
-
-                            )
-            if (status==="unanwsered") 
+    if (status==="anwsered") 
             return (
-                    <div> 
-                        <p>unanwsered </p>
+            
+            <Card >
+             <Card.Content compact>  
+                <Image src={img} floated="left" size='tiny' circular />                   
+            
+                <Card.Header>{CurrentQuestion.author +' asks'}</Card.Header>
+                <Card.Description>                          
+                <Form onSubmit={handleSubmit}>
+                    <div>
+                        <Label>
+                        <Input type="radio" value="optionOne" checked={option == 'optionOne'} 
+                                onChange={handleChange} />
+                        <span>{CurrentQuestion.optionOne.text}</span>
+                        </Label>
+                        <Label>
+                        <Input type="radio" value="optionTwo" checked={option == 'optionTwo'} 
+                                onChange={handleChange} />
+                        <span>{CurrentQuestion.optionTwo.text}</span>
+                        </Label>
                     </div>
-                        )
+                    <Button type="submit">Submit</Button>
+                    </Form>
+                </Card.Description>   
+             </Card.Content>
+        </Card>
+
+                )
+    if (status==="unanwsered") 
+    return (
+            <div> 
+                <p>unanwsered </p>
+            </div>
+                )
 }
 
      //   function mapStateToProps({ users , questions } , props ) { //props thats passed from Home
