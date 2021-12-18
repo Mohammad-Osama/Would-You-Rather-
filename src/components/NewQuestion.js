432cde46aca30e6ba9f08de78316ce6d17a1ed16
Mohammad <moh.osama84x@gmail.com>
2021-12-19 00:34:51 +0200

    finalizing NewQuestion component, error dispatch

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Form, Input ,Button , Container , Header} from 'semantic-ui-react'
+import { saveNewQuestion } from '../actions/shared'
 
 
 
@@ -8,29 +9,65 @@ import { Form, Input ,Button , Container , Header} from 'semantic-ui-react'
 
 
  class NewQuestion extends Component {
+
+    state = {
+        optionOneText :  '' ,
+        optionTwoText :  ''
+
+            }
+
+            handleChange = (e)=>{
+                e.preventDefault()  
+                this.setState( { [e.target.id] : e.target.value } )
+                console.log (this.state)
+
+            }
+
+    handleSubmit = (e)=>{
+        e.preventDefault()
+        const {optionOneText , optionTwoText } = this.state
+        this.props.dispatch(saveNewQuestion(optionOneText , optionTwoText))
+
+    }
+
+
+
+
+
+
+
     render() {
         return (
            
-                <Form>
+                <Form onSubmit={this.handleSubmit}>
                      <Header size='huge' color = "teal" >Would You Rather !</Header>
                     <Form.Field required>
-                        <label>First Name</label>
-                        <input placeholder='First Name' />
+                        <label>Option One</label>
+                        <input placeholder='Option One'  value={this.state.optionOneText}
+                                                         onChange={this.handleChange} 
+                                                         id = "optionOneText"
+                                                                                       />
                     </Form.Field>
                     <Form.Field required>
-                        <label>Last Name</label>
-                        <input placeholder='Last Name' />
+                        <label>Option Two</label>
+                        <input placeholder='Option Two' value={this.state.optionTwoText}
+                                                         onChange={this.handleChange} 
+                                                         id = "optionTwoText"                />
                     </Form.Field>
                     
                     <Button type='submit'>Save Question</Button>
                 </Form>
-            
+           
         )
     }
 }
 
 
+function mapStateToProps({ authedUser }) {
+    return {
+        authedUser
+    }
+  }
 
 
-
-export default connect()(NewQuestion)
+export default connect(mapStateToProps)(NewQuestion)
